aa9c0d6c5f0354f9947827131a610de77d35bd0c
Андрей <andreyfromilaria@yandex.ru>
2015-04-25 12:41:27 +0300

    Реализовал Set и Get методы для предприятия. Так же добавил функцию проверки принадлежности коэффициента к какой-либо из категорий.

--- Include/IlariaTypes.hxx
+++ Include/IlariaTypes.hxx
@@ -13,6 +13,20 @@ namespace Ilaria
         ERS_HIGH        = 3,
         ERS_VERY_HIGH   = 4,
     };
+    
+    E_RATIO_STATE GetState(real ARatio, real AFirst, real ASecond, real AThird, real AFourth, real AFifth, real ASixth)
+    {
+        if (ARatio >= AFirst && ARatio < ASecond)
+            return ERS_VERY_LOW;
+        else if (ARatio >= ASecond && ARatio < AThird)
+            return ERS_LOW;
+        else if (ARatio >= AThird && ARatio < AFourth)
+            return ERS_MIDDLE;
+        else if (ARatio >= AFourth && ARatio < AFifth)
+            return ERS_HIGH;
+        
+        return ERS_VERY_HIGH;
+    };
 };
 
 #endif
---
+++ Src/CEnterprise.cxx
@@ -0,0 +1,395 @@
+#include "CEnterprise.hxx"
+
+namespace Ilaria
+{
+    real CEnterprise::GetTurnaroundActives()
+    {
+        return _TurnaroundActives;
+    };
+    
+    real CEnterprise::GetStocks()
+    {
+        return _Stocks;
+    };
+    
+    real CEnterprise::GetValueAddedTax()
+    {
+        return _ValueAddedTax;
+    };
+    
+    real CEnterprise::GetReceivables()
+    {
+        return _Receivables;
+    };
+    
+    real CEnterprise::GetCurrentLiability()
+    {
+        return _CurrentLiability;
+    };
+    
+    real CEnterprise::GetDefferedIncome()
+    {
+        return _DefferedIncome;
+    };
+    
+    real CEnterprise::GetLongTermCommitment()
+    {
+        return _LongTermCommitment;
+    };
+    
+    real CEnterprise::GetLoans()
+    {
+        return _Loans;
+    };
+    
+    real CEnterprise::GetCapitalAndReserves()
+    {
+        return _CapitalAndReserves;
+    };
+    
+    real CEnterprise::GetTargetFinancingAndReceipts()
+    {
+        return _TargetFinancingAndReceipts;
+    };
+    
+    real CEnterprise::GetNonCurrentAssets()
+    {
+        return _NonCurrentAssets;
+    };
+    
+    real CEnterprise::GetCurrentAssets()
+    {
+        return _CurrentAssets;
+    };
+    
+    real CEnterprise::GetRevenueFromSaleOfGoods()
+    {
+        return _RevenueFromSaleOfGoods;
+    };
+    
+    real CEnterprise::GetBusinessExpenses()
+    {
+        return _BusinessExpenses;
+    };
+    
+    real CEnterprise::GetAssets()
+    {
+        return _Assets;
+    };
+    
+    real CEnterprise::GetManagementCosts()
+    {
+        return _ManagementCosts;
+    };
+    
+    real CEnterprise::GetPayables()
+    {
+        return _Payables;
+    };
+    
+    real CEnterprise::GetDebtParticipants()
+    {
+        return _DebtParticipants;
+    };
+    
+    real CEnterprise::GetCostOfGoodSold()
+    {
+        return _CostOfGoodSold;
+    };
+    
+    real CEnterprise::GetProfitBeforeTax()
+    {
+        return _ProfitBeforeTax;
+    };
+    
+    real CEnterprise::GetIncomeTax()
+    {
+        return _IncomeTax;
+    };
+    
+    real CEnterprise::GetNonOperatingIncome()
+    {
+        return _NonOperatingIncome;
+    };
+    
+    real CEnterprise::GetUncoveredLoss()
+    {
+        return _UncoveredLoss;
+    };
+    
+    real CEnterprise::GetTotalAssets()
+    {
+        return _TotalAssets;
+    };
+    
+    real CEnterprise::GetBorrowings()
+    {
+        return _Borrowings;
+    };
+    
+    real CEnterprise::GetTotalLiabilities()
+    {
+        return _TotalLiabilities;
+    };
+    
+    real CEnterprise::GetNetWorkingCapital()
+    {
+        return _NetWorkingCapital;
+    };
+    
+    real CEnterprise::GetInterestPayable()
+    {
+        return _InterestPayable;
+    };
+    
+    real CEnterprise::GetMarketValueOfEquity()
+    {
+        return _MarketValueOfEquity;
+    };
+    
+    real CEnterprise::GetCostTotalDebt()
+    {
+        return _CostTotalDebt;
+    };
+    
+    real CEnterprise::GetTangibleAssets()
+    {
+        return _TangibleAssets;
+    };
+    
+    real CEnterprise::GetShortTermliabilities()
+    {
+        return _ShortTermliabilities;
+    };
+    
+    real CEnterprise::GetOperatingAssets()
+    {
+        return _OperatingAssets;
+    };
+    
+    real CEnterprise::GetOperatingExpenses()
+    {
+        return _OperatingExpenses;
+    };
+    
+    real CEnterprise::GetNetProfitOfTheReportingPeriod()
+    {
+        return _NetProfitOfTheReportingPeriod;
+    };
+    
+    real CEnterprise::GetEquity()
+    {
+        return _Equity;
+    };
+    
+    real CEnterprise::GetMostLiqilineAssets()
+    {
+        return _MostLiqilineAssets;
+    };
+    
+    real CEnterprise::GetNetLoss()
+    {
+        return _NetLoss;
+    };
+    
+    real CEnterprise::GetSales()
+    {
+        return _Sales;
+    };
+
+    void CEnterprise::SetTurnaroundActives(const real& AValue)
+    {
+        _TurnaroundActives = AValue;
+    };
+    
+    void CEnterprise::SetStocks(const real& AValue)
+    {
+        _Stocks = AValue;
+    };
+    
+    void CEnterprise::SetValueAddedTax(const real& AValue)
+    {
+        _ValueAddedTax = AValue;
+    };
+    
+    void CEnterprise::SetReceivables(const real& AValue)
+    {
+        _Receivables = AValue;
+    };
+    
+    void CEnterprise::SetCurrentLiability(const real& AValue)
+    {
+        _CurrentLiability = AValue;
+    };
+    
+    void CEnterprise::SetDefferedIncome(const real& AValue)
+    {
+        _DefferedIncome = AValue;
+    };
+    
+    void CEnterprise::SetLongTermCommitment(const real& AValue)
+    {
+        _LongTermCommitment = AValue;
+    };
+    
+    void CEnterprise::SetLoans(const real& AValue)
+    {
+        _Loans = AValue;
+    };
+    
+    void CEnterprise::SetCapitalAndReserves(const real& AValue)
+    {
+        _CapitalAndReserves = AValue;
+    };
+    
+    void CEnterprise::SetTargetFinancingAndReceipts(const real& AValue)
+    {
+        _TargetFinancingAndReceipts = AValue;
+    };
+    
+    void CEnterprise::SetNonCurrentAssets(const real& AValue)
+    {
+        _NonCurrentAssets = AValue;
+    };
+    
+    void CEnterprise::SetCurrentAssets(const real& AValue)
+    {
+        _CurrentAssets = AValue;
+    };
+    
+    void CEnterprise::SetRevenueFromSaleOfGoods(const real& AValue)
+    {
+        _RevenueFromSaleOfGoods = AValue;
+    };
+    
+    void CEnterprise::SetBusinessExpenses(const real& AValue)
+    {
+        _BusinessExpenses = AValue;
+    };
+    
+    void CEnterprise::SetAssets(const real& AValue)
+    {
+        _Assets = AValue;
+    };
+    
+    void CEnterprise::SetManagementCosts(const real& AValue)
+    {
+        _ManagementCosts = AValue;
+    };
+    
+    void CEnterprise::SetPayables(const real& AValue)
+    {
+        _Payables = AValue;
+    };
+    
+    void CEnterprise::SetDebtParticipants(const real& AValue)
+    {
+        _DebtParticipants = AValue;
+    };
+    
+    void CEnterprise::SetCostOfGoodSold(const real& AValue)
+    {
+        _CostOfGoodSold = AValue;
+    };
+    
+    void CEnterprise::SetProfitBeforeTax(const real& AValue)
+    {
+        _ProfitBeforeTax = AValue;
+    };
+    
+    void CEnterprise::SetIncomeTax(const real& AValue)
+    {
+        _IncomeTax = AValue;
+    };
+    
+    void CEnterprise::SetNonOperatingIncome(const real& AValue)
+    {
+        _NonOperatingIncome = AValue;
+    };
+    
+    void CEnterprise::SetUncoveredLoss(const real& AValue)
+    {
+        _UncoveredLoss = AValue;
+    };
+    
+    void CEnterprise::SetTotalAssets(const real& AValue)
+    {
+        _TotalAssets = AValue;
+    };
+    
+    void CEnterprise::SetBorrowings(const real& AValue)
+    {
+        _Borrowings = AValue;
+    };
+    
+    void CEnterprise::SetTotalLiabilities(const real& AValue)
+    {
+        _TotalLiabilities = AValue;
+    };
+    
+    void CEnterprise::SetNetWorkingCapital(const real& AValue)
+    {
+        _NetWorkingCapital = AValue;
+    };
+    
+    void CEnterprise::SetInterestPayable(const real& AValue)
+    {
+        _InterestPayable = AValue;
+    };
+    
+    void CEnterprise::SetMarketValueOfEquity(const real& AValue)
+    {
+        _MarketValueOfEquity = AValue;
+    };
+    
+    void CEnterprise::SetCostTotalDebt(const real& AValue)
+    {
+        _CostTotalDebt = AValue;
+    };
+    
+    void CEnterprise::SetTangibleAssets(const real& AValue)
+    {
+        _TangibleAssets = AValue;
+    };
+    
+    void CEnterprise::SetShortTermliabilities(const real& AValue)
+    {
+        _ShortTermliabilities = AValue;
+    };
+    
+    void CEnterprise::SetOperatingAssets(const real& AValue)
+    {
+        _OperatingAssets = AValue;
+    };
+    
+    void CEnterprise::SetOperatingExpenses(const real& AValue)
+    {
+        _OperatingExpenses = AValue;
+    };
+    
+    void CEnterprise::SetNetProfitOfTheReportingPeriod(const real& AValue)
+    {
+        _NetProfitOfTheReportingPeriod = AValue;
+    };
+    
+    void CEnterprise::SetEquity(const real& AValue)
+    {
+        _Equity = AValue;
+    };
+    
+    void CEnterprise::SetMostLiqilineAssets(const real& AValue)
+    {
+        _MostLiqilineAssets = AValue;
+    };
+    
+    void CEnterprise::SetNetLoss(const real& AValue)
+    {
+        _NetLoss = AValue;
+    };
+    
+    void CEnterprise::SetSales(const real& AValue)
+    {
+        _Sales = AValue;
+    };
+    
+};
